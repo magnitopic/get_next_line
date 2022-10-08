6f0a5f89e71928a0b3b95be8fb655b5d848a8c53
magnitopic <magnitopic@gmail.com>
2022-10-08 12:58:09 +0200

    reading file contents

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,27 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 17:35:54 by alaparic          #+#    #+#             */
-/*   Updated: 2022/10/07 17:36:24 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/10/08 12:57:19 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	buffer[2048];
+	int		bytes;
+
+	bytes = read(fd, buffer, 2048);
+	printf("%s", buffer);
+	return (0);
+}
+
+int main(void)
+{
+	int	file;
+
+	file = open("test.txt", O_RDONLY);
+	get_next_line(file);
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,17 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 17:36:30 by alaparic          #+#    #+#             */
-/*   Updated: 2022/10/07 17:36:32 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/10/08 12:52:09 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*get_next_line(int fd);
+#endif
