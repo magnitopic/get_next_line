4cb5edb9bab3be3a96f929a92ac80a5a6fc7f1c3
magnitopic <magnitopic@gmail.com>
2022-10-17 20:20:19 +0200

    start of buffer memory managment

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 17:35:54 by alaparic          #+#    #+#             */
-/*   Updated: 2022/10/17 17:12:13 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/10/17 20:19:45 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,35 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
+static int	expand_buffer(int fd, char *buffer)
+{
+	char	*aux;
+	int		len;
+	int		i;
+
+	aux = buffer;
+	*buffer = (char *) malloc(BUFFER_SIZE * sizeof(char));
+	len = read(fd, aux, BUFFER_SIZE);
+	i = len;
+	while (i--)
+		buffer[i] = aux[i];
+	return (len);
+}
+
 static char	*expand_str(char *str)
 {
 	char	*new_str;
 	int		len;
 
 	len = ft_strlen(str);
-	new_str = calloc((BUFFER_SIZE + len + 1), sizeof(char));
+	new_str = malloc((BUFFER_SIZE + len + 1) * sizeof(char));
 	while (len != 0)
 	{
 		new_str[len - 1] = str[len - 1];
 		len--;
 	}
 	free(str);
+	new_str[BUFFER_SIZE] = 0;
 	return (new_str);
 }
 
@@ -46,20 +62,25 @@ char	*get_next_line(int fd)
 	char			*str;
 	int				len;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (0);
 	if (!buffer[i])
 	{
-		len = read(fd, buffer, BUFFER_SIZE);
+		expand_buffer(fd, buffer);
 		i = 0;
 	}
 	j = 0;
 	str = calloc((BUFFER_SIZE + 1), sizeof(char));
-	while (buffer[i] != '\n' && len >= i)
+	while (buffer[i] != '\n' && len != 0)
 	{
 		if (!buffer[i])
 		{
-			len = read(fd, buffer, BUFFER_SIZE);
+			expand_buffer(fd, buffer);
+			/* if (len == 0)
+			{
+				free(buffer);
+				break ;
+			} */
 			i = 0;
 			str = expand_str(str);
 		}
@@ -68,6 +89,7 @@ char	*get_next_line(int fd)
 		i++;
 	}
 	str[j] = buffer[i];
+	//printf("%d",buffer[i]);
 	i++;
 	return (str);
 }
