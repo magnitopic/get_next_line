364288b1bc42da45a81345bd2af8a4387b8f4156
magnitopic <magnitopic@gmail.com>
2022-10-24 16:32:36 +0200

    header with new fucntions

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 17:36:30 by alaparic          #+#    #+#             */
-/*   Updated: 2022/10/08 16:47:32 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/10/24 16:32:24 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <fcntl.h>
 # include <unistd.h>
-# include <stdio.h>
 # include <stdlib.h>
 
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
 char	*get_next_line(int fd);
 #endif
