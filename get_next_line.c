5f2f09192fb7c4d1d3ff285a6946dfcc6c03b494
magnitopic <magnitopic@gmail.com>
2022-10-27 20:12:22 +0200

    still memory errors

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 17:35:54 by alaparic          #+#    #+#             */
-/*   Updated: 2022/10/26 19:39:10 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/10/27 20:09:57 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 static char	*join_buffer(char *buffer, char *new_str)
 {
-	
+	char	*aux;
+
+	aux = ft_strjoin(buffer, new_str);
+	free(buffer);
+	return (aux);
 }
 
 static char	*expand_buffer(int fd, char *buffer)
@@ -24,26 +28,25 @@ static char	*expand_buffer(int fd, char *buffer)
 
 	if (!buffer)
 		buffer = ft_calloc(1, 1);
-	aux = calloc((BUFFER_SIZE + 1), sizeof(char));
+	aux = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	len = 1;
 	while (len != 0)
 	{
 		len = read(fd, aux, BUFFER_SIZE);
 		aux[len] = '\0';
-		buffer = ;
-		if (ft_strchr(aux,'\n') != NULL)
+		buffer = join_buffer(buffer, aux);
+		if (ft_strchr(aux, '\n') != NULL)
 			break ;
 	}
 	aux = "";
-	free(buffer);
 	buffer = (char *) malloc(BUFFER_SIZE * sizeof(char));
 	len = read(fd, aux, BUFFER_SIZE);
 	buffer = ft_strjoin(aux, buffer);
 	free(aux);
-	return (len);
+	return (buffer);
 }
 
-static char	*expand_str(char *str)
+/* static char	*expand_str(char *str)
 {
 	char	*new_str;
 	char	*aux;
@@ -52,25 +55,25 @@ static char	*expand_str(char *str)
 	new_str = ft_strjoin(str, aux);
 	free(str);
 	return (new_str);
-}
+} */
 
 char	*get_next_line(int fd)
 {
 	static char		*buffer;
-	char			*str;
-	int				len;
+	/* char			*str; */
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (0);
-	buffer=expand_buffer();
-	return (str);
+	buffer=expand_buffer(fd, buffer);
+	printf("%s", buffer);
+	return NULL;
 }
 
 int	main(void)
 {
 	int	file;
 	file = open("test.txt", O_RDONLY);
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 2; i++)
 		printf("%s", get_next_line(file));
 		//get_next_line(file);
 	return (0);
